1f61e0c0c0032c7238e9ef31edfd011d1dcbd723
Jiseuk Jeong <jiseuk.jeong@slz.kr>
2024-07-03 08:56:29 +0900

    csrf Exception handled

--- djangogram/users/views.py
+++ djangogram/users/views.py
@@ -1,8 +1,23 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import DetailView
+from django.views.generic import RedirectView
+from django.views.generic import UpdateView
+
+from djangogram.users.models import User
+
+
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
 def main(request):
     if request.method == "GET":
         return render(request, "users/main.html")
@@ -15,3 +30,38 @@ def main(request):
             return HttpResponseRedirect(reverse("posts:index"))
         else:
             return render(request, "users/main.html")
+
+# class UserDetailView(LoginRequiredMixin, DetailView):
+#     model = User
+#     slug_field = "username"
+#     slug_url_kwarg = "username"
+
+
+# user_detail_view = UserDetailView.as_view()
+
+
+# class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+#     model = User
+#     fields = ["name"]
+#     success_message = _("Information successfully updated")
+
+#     def get_success_url(self):
+#         # for mypy to know that the user is authenticated
+#         assert self.request.user.is_authenticated
+#         return self.request.user.get_absolute_url()
+
+#     def get_object(self):
+#         return self.request.user
+
+
+# user_update_view = UserUpdateView.as_view()
+
+
+# class UserRedirectView(LoginRequiredMixin, RedirectView):
+#     permanent = False
+
+#     def get_redirect_url(self):
+#         return reverse("users:detail", kwargs={"username": self.request.user.username})
+
+
+# user_redirect_view = UserRedirectView.as_view()
